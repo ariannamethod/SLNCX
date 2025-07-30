1d7c088f8efaa4cf756335bc89938a597f9c3371
Arianna Method <theariannamethod@gmail.com>
2025-07-30 03:46:14 +0300

    feat: make checkpoint path configurable

--- wulf_cli.py
+++ wulf_cli.py
@@ -1,12 +1,22 @@
 import argparse
-from wulf_inference import generate
+import os
+from pathlib import Path
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description='Wulf1 CLI interface')
     parser.add_argument('prompt', help='prompt text')
+    parser.add_argument('--ckpt', help='path to checkpoint file')
     args = parser.parse_args()
-    print(generate(args.prompt))
+
+    if args.ckpt:
+        os.environ['CKPT_PATH'] = args.ckpt
+
+    import wulf_inference
+    if args.ckpt:
+        wulf_inference.CKPT_PATH = Path(args.ckpt)
+
+    print(wulf_inference.generate(args.prompt))
 
 
 if __name__ == '__main__':
